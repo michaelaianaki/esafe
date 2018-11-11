50f01fd3961ad0e565cc4e0f87462c1687f3daf2
Michaela Ianaki <michaelaianaki@Michaelas-MacBook-Air.local>
2018-11-11 01:54:24 -0500

    adding ios files

---
+++ gunViolence/gunViolence/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  gunViolence
+//
+//  Created by Michaela Ianaki on 11/11/18.
+//  Copyright © 2018 Michaela Ianaki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ gunViolence/gunViolence/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  gunViolence
+//
+//  Created by Michaela Ianaki on 11/11/18.
+//  Copyright © 2018 Michaela Ianaki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
